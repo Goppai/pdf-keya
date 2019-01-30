21ec8b61ec70a41c6dee448ea13a17cd311af4a0
ningl <ningl@curacloudcorp.com>
2019-01-30 18:26:05 +0800

    change webpack intent target index.js path

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
@@ -155,7 +153,7 @@ module.exports = function(webpackEnv) {
       //   require.resolve('./reference/webpackHotDevClient'),
       // Finally, this is your app's code:
       app: [paths.appIndexJs],
-      intents: hasIntents ? [paths.appIndexJs] : []
+      intents: hasIntents ? [paths.intentIndexJs] : []
       // We include the app code last so that if there is a runtime error during
       // initialization, it doesn't blow up the WebpackDevServer client, and
       // changing JS code would still trigger a refresh.
