9092b17360f5a7788901e286df00cbf429e00609
ningl <ningl@curacloudcorp.com>
2019-07-24 19:35:42 +0800

    T5689: delete unused investigating code

--- src/dataapi/other_forms/resolvers.js
+++
@@ -1,9 +0,0 @@
-import { getOrCreateMgr } from './mgr';
-
-const root = {
-  getMgr({ type }) {
-    return getOrCreateMgr(type);
-  },
-};
-
-export default root;
--- src/dataapi/other_forms/schema.js
+++
@@ -1,7 +0,0 @@
-import { buildSchema } from 'graphql';
-
-import def from './schema_def.gql'; // eslint-disable-line import/no-webpack-loader-syntax
-
-const schema = buildSchema(def);
-
-export default schema;
